fc64bf2e9174615b39b256754759b9a8b11d54fd
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-27 14:31:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+ function removeColor() {
+    const selectElement = document.getElementById("colorSelect");
+    const selectedValue = selectElement.value;
+
+    // Find the option with the selected value and remove it
+    const optionToRemove = selectElement.querySelector(`option[value="${selectedValue}"]`);
+    optionToRemove.remove();
+  }
